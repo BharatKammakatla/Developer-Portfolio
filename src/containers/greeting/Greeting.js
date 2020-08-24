a9a03f8e6a4aee4d8d73a16a5522291d29a291e4
Bharat Kammakatla <bharatkammakatla@gmail.com>
2020-08-24 22:02:15 +0800

    updated css and main theme color

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -28,7 +28,7 @@ export default function Greeting() {
           </div>
         </div>
         <div className="greeting-image-div">
-          <img alt="man at desk" src={require("../../assets/images/manOnTable.svg")}></img>
+          <img alt="man at desk" src={require("../../assets/images/manOnTable2.svg")}></img>
         </div>
       </div>
     </div>
