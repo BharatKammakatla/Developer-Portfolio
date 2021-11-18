1bcad2bd0d25fdcd2ba4ed97b4dda38ab55eae89
Fede Conte Grand <fede.contegrand@gmail.com>
2021-11-18 03:07:01 -0300

    r

--- fetch.js
+++ fetch.js
@@ -52,7 +52,7 @@ const default_options = {
   port: 443,
   method: "POST",
   headers: {
-    Authorization: `Bearer ${REACT_APP_GITHUB_TOKEN }`,
+    Authorization: `token ${REACT_APP_GITHUB_TOKEN }`,
     "User-Agent": "Node",
   },
 };
